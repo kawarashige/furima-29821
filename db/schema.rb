1de132cd12010a5d778bb9bcfc190a939b77d66b
kawarashige <kawarashige23@gmail.com>
2020-09-20 17:50:59 +0900

    Active Storageの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_081601) do
+ActiveRecord::Schema.define(version: 2020_09_20_084516) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
