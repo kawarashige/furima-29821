237a35c892c1066853970ce552094eec58f62606
kawarashige <kawarashige23@gmail.com>
2020-09-20 17:11:29 +0900

    ユーザーモデルの作成

---
+++ db/migrate/20200920080550_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :last_name,           null: false
+      t.string :last_name_furigana,  null: false
+      t.string :first_name,          null: false
+      t.string :first_name_furigana, null: false
+      t.string :email,               null: false
+      t.string :password,            null: false
+      t.string :nickname,            null: false
+      t.date   :birth_day,           null: false
+      t.timestamps
+    end
+  end
+end
