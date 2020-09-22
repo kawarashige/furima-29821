8f5f6e6bb4624af59174fffb03101ebe6820446a
kawarashige <kawarashige23@gmail.com>
2020-09-22 09:29:00 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,20 +15,17 @@ class User < ApplicationRecord
   # validates :password, presence: true
 
   with_options presence: true do
-
     validates           :nickname
     validates           :email,             uniqueness: true
     validates           :password,          length: { minimum: 6 }
 
     PASSWORD = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password,          with: PASSWORD, message: "Include both letters and numbers"
+    validates_format_of :password,          with: PASSWORD, message: 'Include both letters and numbers'
 
-    validates           :first_name,        format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
-    validates           :last_name,         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
-    validates           :first_name_kana,   format: { with: /\A[ァ-ヶ一ー]+\z/, message: "Full-width katakana characters" }
-    validates           :last_name_kana,    format: { with: /\A[ァ-ヶ一ー]+\z/, message: "Full-width katakana characters" }
+    validates           :first_name,        format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+    validates           :last_name,         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+    validates           :first_name_kana,   format: { with: /\A[ァ-ヶ一ー]+\z/, message: 'Full-width katakana characters' }
+    validates           :last_name_kana,    format: { with: /\A[ァ-ヶ一ー]+\z/, message: 'Full-width katakana characters' }
     validates           :birth_day
-    
   end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,12 @@
 FactoryBot.define do
-
   factory :user do
-    nickname        {"furima十郎"}
-    email           {"sample10@gmail.com"}
-    password        {"101010jjj"}
-    first_name      {"山田"}
-    last_name       {"十郎"}
-    first_name_kana {"ヤマダ"}
-    last_name_kana  {"ジュウロウ"}
-    birth_day       {"1955-04-27"}
+    nickname        { 'furima十郎' }
+    email           { 'sample10@gmail.com' }
+    password        { '101010jjj' }
+    first_name      { '山田' }
+    last_name       { '十郎' }
+    first_name_kana { 'ヤマダ' }
+    last_name_kana  { 'ジュウロウ' }
+    birth_day       { '1955-04-27' }
   end
-  
 end
