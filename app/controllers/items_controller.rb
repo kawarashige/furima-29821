f1ffc8d6648e7df3f97ede02c914f833eaef97fe
kawarashige <kawarashige23@gmail.com>
2020-09-22 23:30:31 +0900

    views/new.html.erbの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order('created_at DESC')
   end
+
+
+  def new
+  end
+
+  def created
+  end
+
+  private
+
+  def item_params
+
+  end
 end
