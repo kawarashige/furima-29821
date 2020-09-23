92b3578f429e43548dccd1846876af9a893a837e
kawarashige <kawarashige23@gmail.com>
2020-09-24 06:00:00 +0900

    エラーハンドリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   with_options presence: true do
+    validates           :image
     validates           :name
     validates           :info
     validates           :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
@@ -19,6 +20,7 @@ class Item < ApplicationRecord
     validates           :category_id
     validates           :sales_status_id
     validates           :shipping_fee_status_id
+    validates           :prefecture_id
     validates           :scheduled_delivery_id
   end
 
