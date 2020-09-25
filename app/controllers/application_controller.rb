daf773656063b6386f236671c19dc21f1c648bb9
kawarashige <kawarashige23@gmail.com>
2020-09-25 09:35:46 +0900

    商品情報編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # def after_sign_in_path_for(resource)
+  #   items_new_path
+  # end
+
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :last_name_kana, :first_name, :first_name_kana, :nickname, :birth_day])
   end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
 
   def new
     @item = Item.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,7 +22,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -25,4 +38,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :prefecture_id, :category_id, :sales_status_id, :shipping_fee_status_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
