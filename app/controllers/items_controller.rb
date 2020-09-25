6da6167f97cdd260cd658563704d866fbb360b14
kawarashige <kawarashige23@gmail.com>
2020-09-25 14:58:11 +0900

    authenticate_user!の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
