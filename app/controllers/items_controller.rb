fc94a0cc0e115091202613dd1ac7a662ad43be0e
kawarashige <kawarashige23@gmail.com>
2020-09-25 17:48:03 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,7 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path if @item.destroy
+    if @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
@@ -49,6 +51,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index if user_signed_in? && @item.user.id != current_user.id
+    if user_signed_in? && @item.user.id != current_user.id
+      redirect_to action: :index
+    end
   end
 end
