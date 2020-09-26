032bc1c9e4517dd0fac8496d9a38b948f89ae813
kawarashige <kawarashige23@gmail.com>
2020-09-26 12:46:50 +0900

    orderビューファイルの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+  end
+
+  private
+
+  def order_params
+    params.permit(:price, :token)
+  end
+
 end
---
+++ app/javascript/card.js
@@ -0,0 +1,24 @@
+const pay = () => {
+  Payjp.setPublicKey("");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      console.log(status)
+      console.log(response)
+    })
+
+  })
+}
+
+window.addEventListener("load", pay);
