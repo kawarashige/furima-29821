533bab42f979ca03d3607b85c61f4d169ed2e327
kawarashige <kawarashige23@gmail.com>
2020-09-26 16:28:11 +0900

    READ.MEとマイグレーションファイルの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,10 @@
 class Order < ApplicationRecord
+
   belongs_to :user
   belongs_to :item
+  has_one    :address
 
   attr_accessor :token
-  validates :price, presence: true
   validates :token, presence: true
+
 end
