8f162ea5626aba4b4cf33bf885153fae02949076
kawarashige <kawarashige23@gmail.com>
2020-09-26 17:20:47 +0900

    カラムの指定

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,4 +1,15 @@
 class OrderAddress
 
   include ActiveModel::ActiveModel
-  attr_accessor :token,
+  attr_accessor :token
+
+  with_options presence: true do
+    validates :token
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :addresses
+    validates :phone_number
+  end
+  
+end
